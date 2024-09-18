f01379d1f7ed29779db0c3ac5d4686f6e05eb0f8
rooler <roel.wier@gmail.com>
2024-09-17 22:47:48 -0400

    Reproduce skeleton

---
+++ reproduce.py
@@ -0,0 +1,48 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from vff import run_1d, run_evolution
+
+
+def figure_3a():
+    return
+
+
+def figure_3b():
+    return
+
+
+def figure_3c():
+    return
+
+
+def figure_4a():
+    return
+
+
+def figure_4b():
+    return
+
+
+def figure_4c():
+    return
+
+
+def figure_5():
+    return
+
+
+def figure_6a():
+    return
+
+
+def figure_6b():
+    return
+
+
+def figure_6c():
+    return
+
+
+if __name__ == '__main__':
+    figure_3a()
